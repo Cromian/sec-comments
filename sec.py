f7f84a1b476b596e79395e21699989a98e4baf49
Misha <Cromian@users.noreply.github.com>
2025-07-26 19:02:03 -0400

    Update sec.py

--- sec.py
+++ sec.py
@@ -64,7 +64,7 @@ links = table.find_all("a");
 file_data = "[a['href'] for a in links if a.has_attr('href')]
 
 # This is the output of a single docket comment value
-data = {"id": docket_id, "title": docket_title, "file_data": file_data];
+data = {"id": docket_id, "title": docket_title, "file_data": file_data};
 
 # Output as JSON
 print(json.dumps(data));
